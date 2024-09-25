3203b6d72f19071785dd004a72b0a8923c65a615
Subrata Shil <155966227+Subrata3dx@users.noreply.github.com>
2024-09-25 13:37:48 +0530

    Updated it with comments

--- Interview_preparations_C++/Armstrong_number_finding/main.c
+++ Interview_preparations_C++/Armstrong_number_finding/main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
-int getPow(int x, int y) //returns power of x^y
+//returns the value of x^y
+int getPow(int x, int y) 
 {
 if(y==0)
 return 1;
@@ -10,46 +11,46 @@ else
 return x*getPow(x, (y-1));
 }
 
-int getDig(int y) //returns how many digits are there in the int argument
+//returns how many digits are there in the int argument
+int getDig(int y) 
 {
     int num = 0;
     while(y>0)
-
     {
         y=y/10;
         num++;
-
     }
     return num;
 
 }
-int getSum(int s) //returns the sum of each digit's raised to the power of the number of the digits in the input
-{
-   int x =0, k, p, t;
-   t=s;
-   k = getDig(s);
-   while(t>0)
-    {
-       p=t%10;
-       x +=getPow(p, k);
-       t=t/10;
 
+//The following function returns the sum total of each digit's raised to the power of the number of the digits in the input
+int getSum(int s) 
+{
+   int sum =0, digit, mod, temp;
+   temp=s; 
+   digit = getDig(s); //get number of digits
+   while(temp>0)
+    {
+       mod=temp % 10;
+       sum += getPow(mod, digit); //returns mod^digit
+       temp=temp/10;
     }
-return x;
+return sum;
 }
 
+//driver function
 int main()
 {
-int n, i, sum;
+int num, sum;
 printf("Enter a value to check if there are some Armstrong value starting from 0:");
-scanf("%d", &n);
-for(i= 0; i<n;i++)
+scanf("%d", &num);
+//checks from 0 to num for armstrong values
+for(i= 0; i<num;i++)
 {
-    sum = getSum(i);
+    sum = getSum(i); //get sum total of each digit's raised to the power of number of digits in i
     if(sum==i)
-        printf("\n%d is an Armstrong number.", i);
-
-
+    printf("\n%d is an Armstrong number.", i);
 }
 return 0;
 }
